e08930587e29ebb26b45fe82b99570e6b09525c7
Daniel Quintero <dquintero.ing@gmail.com>
2023-05-09 19:32:32 -0500

    animaciones de texto

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    console.log("Hello World!");  
+    console.log("Hello World!");
     // Obtener el elemento de audio
-    var audio = document.getElementById('my-audio');
-    audio.play();
+    // var audio = document.getElementById('my-audio');
+    // audio.play();
 
     // $('#navbarResponsive ul li a').on("click", function(){
     //     $('#btn_menu').click();
@@ -20,9 +20,35 @@ $(document).ready(function () {
             }
         });
     });
-    
+
+
+    const elementosAnimados = document.querySelectorAll('.title');
+
+    const observador = new IntersectionObserver((entradas) => {
+        entradas.forEach((entrada) => {
+            if (entrada.isIntersecting) {
+                const indice = Array.from(elementosAnimados).indexOf(entrada.target);
+                animarElemento(entrada.target, indice);
+                //observador.unobserve(entrada.target);
+            }
+        });
+    });
+
+    elementosAnimados.forEach((elemento) => {
+        observador.observe(elemento);
+    });
+
 });
 
+const animarElemento = (elemento, indice) => {
+    const animacion = elemento.dataset.animacion;
+    elemento.classList.add(`animado-${indice}`, 'animate__animated', `animate__${animacion}`);
+
+    // Eliminar la clase de animación después de que la animación haya terminado
+    elemento.addEventListener('animationend', () => {
+        elemento.classList.remove(`animado-${indice}`, 'animate__animated', `animate__${animacion}`);
+    }, { once: true });
+}
 
 
 
@@ -31,32 +57,33 @@ var countDownDate = new Date("Aug 19, 2023 16:30:00").getTime();
 // var countDownDate = new Date("May 6, 2023 17:39:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-// Get today's date and time
-var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-// Find the distance between now and the count down date
-var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-// Time calculations for days, hours, minutes and seconds
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// Display the result in the element with id="demo"
-document.getElementById("days").innerHTML = days;
-document.getElementById("Hours").innerHTML = hours;
-document.getElementById("Minutes").innerHTML = minutes;
-document.getElementById("Seconds").innerHTML = seconds;
+    // Display the result in the element with id="demo"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("Hours").innerHTML = hours;
+    document.getElementById("Minutes").innerHTML = minutes;
+    document.getElementById("Seconds").innerHTML = seconds;
 
-// If the count down is finished, write some text
-if (distance < 0) {
-    clearInterval(x);
-    const countdown = document.getElementById("countdown");
-    const es_hoy = document.getElementById("es-hoy");
-    countdown.style.display = "none";
-    es_hoy.style.display = "block";
-}
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        const countdown = document.getElementById("countdown");
+        const es_hoy = document.getElementById("es-hoy");
+        countdown.style.display = "none";
+        es_hoy.style.display = "block";
+    }
 }, 1000);
+
